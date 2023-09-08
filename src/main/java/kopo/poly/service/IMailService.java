57d31355480a0fc1a092cfe61d9bd09340079021
chlwnstkd <jun1234755@naver.com>
2023-09-08 15:05:37 +0900

    2023-09-08

---
+++ src/main/java/kopo/poly/service/IMailService.java
@@ -0,0 +1,16 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MailDTO;
+import kopo.poly.dto.NoticeDTO;
+
+import java.util.List;
+
+public interface IMailService {
+
+    //메일 발송
+    int doSendMail(MailDTO pDTO);
+
+    List<MailDTO> getMailList() throws Exception;
+
+    void insertMailInfo(MailDTO pDTO) throws Exception;
+}
