0f9106bdf2a5009170ed884d8d813ca6d6eed94d
chlwnstkd <jun1234755@naver.com>
2023-11-27 13:39:34 +0900

    2023-11-27

--- src/main/java/kopo/poly/dto/PapagoDTO.java
+++ src/main/java/kopo/poly/dto/PapagoDTO.java
@@ -4,11 +4,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
+@Getter
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class PapagoDTO {
 
-    private String langCode;
-    private String text;
+    private String langCode; // 원문의 언어(한국어 : ko / 영어 :en)
+    private String text; // 분석을 위한 문장
+
+    private String translatedText; // 번역된 문장
+    private String scrLangType; // 번역전 문장 사용 언어
+    private String tarLangType; // 번연된 문장 사용 언어
+
+
 }
--- src/main/java/kopo/poly/service/impl/PapagoService.java
+++ src/main/java/kopo/poly/service/impl/PapagoService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Slf4j
@@ -62,6 +63,82 @@ public class PapagoService implements IPapagoService {
         rDTO.setText(text);
 
         log.info(this.getClass().getName() + ".detectLangs End!");
+
+        return rDTO;
+    }
+
+    @Override
+    public PapagoDTO translate(PapagoDTO pDTO) throws Exception {
+
+        log.info(this.getClass().getName() + ".translate Start!");
+
+        // 언어 종류 찾기
+        PapagoDTO rDTO = this.detectLangs(pDTO);
+
+        // 찾은 언어 종류
+        String langCode = CmmUtil.nvl(rDTO.getLangCode());
+
+        rDTO = null; // 사용 용도가 끝나서 메모리에서 지우기
+
+        String source = ""; // 원문 언어(한국어 : ko / 영어 : en)
+        String target = ""; // 번역할 언어
+
+        if (langCode.equals("ko")) {
+            source = "ko";
+            target = "en";
+
+        } else if (langCode.equals("en")) {
+            source = "en";
+            target = "ko";
+
+        } else {
+            // 한국어와 영어가 아니면, 에러 발생시키기
+            new Exception("한국어와 영어만 번역됩닌다.");
+        }
+
+        String text = CmmUtil.nvl(pDTO.getText()); // 번역할 문장
+
+        // 한국어를 영어로 번역하기 위한 파라미터 설정
+        String postParams = "source=" + source + "&target=" + target + "&text=" + URLEncoder.encode(text, "UTF-8");
+
+        log.info("postParams : "+ postParams);
+
+        // PapagoAPI 호출하기
+        String json = NetworkUtil.post(IPapagoService.translateApiURL, this.setNaverInfo(), postParams);
+
+        log.info("json : " + json);
+
+        // JSON 구조를 Map 데이터 구조로 변경하기
+        // 키와 값 구조의 JSON구조로부터 데이터를 쉽게 가져오기 위해 Map 데이터구조로 변경함
+        Map<String, Object> rMap = new ObjectMapper().readValue(json, LinkedHashMap.class);
+
+        // 결과 내용 중 message 정보가져오기
+        Map<String, Object> messageMap = (Map<String, Object>) rMap.get("message");
+
+        // message 결과 내용 중 result 정보가져오기
+        Map<String, String> resultMap = (Map<String, String>) messageMap.get("result");
+
+        log.info("resultMap : " + resultMap);
+
+        String srcLangType = CmmUtil.nvl(resultMap.get("srcLangType"));
+        String tarLangType = CmmUtil.nvl(resultMap.get("tarLangType"));
+        String translatedText = CmmUtil.nvl(resultMap.get("translatedText"));
+
+        log.info("srcLangType : " + srcLangType);
+        log.info("tarLangType : " + tarLangType);
+        log.info("translatedText : " + translatedText);
+
+        rDTO = new PapagoDTO();
+        rDTO.setText(text);
+        rDTO.setTranslatedText(translatedText);
+        rDTO.setScrLangType(srcLangType);
+        rDTO.setTarLangType(tarLangType);
+
+        resultMap = null;
+        messageMap = null;
+        rMap = null;
+
+        log.info(this.getClass().getName() + ".translate End!");
 
         return rDTO;
     }
